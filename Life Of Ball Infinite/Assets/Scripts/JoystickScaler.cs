e18611ba6cf28dcd9210e7335b99def91dadc233
TanaySinghal <singhal.tanay@gmail.com>
2015-08-09 22:52:17 -0400

    v.1.0

---
+++ Life Of Ball Infinite/Assets/Scripts/JoystickScaler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class JoystickScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		float multiplier = 1f;
+		float movementRangeMultiplier = 1f;
+		if(Screen.height > 600) {
+			multiplier = 1.5f;
+			movementRangeMultiplier = 1.5f;
+		}
+		if(Screen.height > 1000) {
+			multiplier = 2.5f;
+			movementRangeMultiplier = 2f;
+		}
+		if(Screen.height > 1400) {
+			multiplier = 3.5f;
+			movementRangeMultiplier = 3f;
+		}
+		//Menu.multiplier
+		RectTransform rt = GetComponent<RectTransform>();
+
+		Vector2 offset = rt.anchoredPosition - rt.sizeDelta/2;
+		offset *= multiplier;
+		rt.sizeDelta *= multiplier;
+		rt.anchoredPosition = rt.sizeDelta/2 + offset;
+
+		Text jump = transform.GetComponentInChildren<Text>();
+		
+		Joystick joystick = GetComponent<Joystick>();
+		if(joystick != null) {
+			joystick.MovementRange = (int)(joystick.MovementRange*movementRangeMultiplier);
+		}
+
+		if(jump != null) {
+			jump.fontSize = (int)(0.9*rt.sizeDelta.y);
+		}
+	}
+}
